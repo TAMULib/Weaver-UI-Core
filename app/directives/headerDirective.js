3b6a16e0591d83555d8c93b7da867a199497da6c
William Welling <wwelling110381@outlook.com>
2015-08-21 08:59:22 -0500

    Actually add header directive.

---
+++ app/directives/headerDirective.js
@@ -0,0 +1,11 @@
+core.directive('header', function ($controller) {
+	return {
+		templateUrl: 'bower_components/core/app/views/header.html',
+		restrict: 'E',
+		replace: false,
+		transclude: true,
+		link: function ($scope, element, attr) {	    	
+			$scope.attr = attr;			
+	    }
+	};
+});

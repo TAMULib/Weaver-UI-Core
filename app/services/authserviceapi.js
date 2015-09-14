5bc530e425aa3ecb4122bb9b4a6ca525bfac8b47
William Welling <wwelling110381@outlook.com>
2015-09-14 13:21:50 -0500

    Updated auth service api.

--- app/services/authserviceapi.js
+++ app/services/authserviceapi.js
@@ -1,4 +1,4 @@
-core.service("AuthServiceApi",function($http, $timeout, StorageService) {
+core.service("AuthServiceApi",function($http, $timeout) {
 
 	var AuthServiceApi = this;
 
@@ -10,7 +10,7 @@ core.service("AuthServiceApi",function($http, $timeout, StorageService) {
 			AuthServiceApi.pendingAssumptions[assume.netid] = $http.get(appConfig.authService+"/admin?netid="+assume.netid,{withCredentials: true}).
 				then(function(response) { 
 					if(response.data.assumed) {
-						StorageService.set('token', response.data.assumed.tokenAsString, 'session');
+						sessionStorage.token = response.data.assumed.tokenAsString;
 					}
 
 					// This timeout ensures that pending request is not nulled to early
@@ -29,7 +29,7 @@ core.service("AuthServiceApi",function($http, $timeout, StorageService) {
 			AuthServiceApi.pendingRefresh = $http.get(appConfig.authService+"/refresh", {withCredentials: true}).
 				then(function(response) {
 					
-						StorageService.set('token', response.data.tokenAsString, 'session');						
+						sessionStorage.token = response.data.tokenAsString;					
 						
 						// This timeout ensures that pending request is not nulled to early
 						$timeout(function() {

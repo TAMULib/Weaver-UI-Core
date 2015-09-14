d3d35a0ab3e516dae7f69e2ea9115032eb76baf5
William Welling <wwelling110381@outlook.com>
2015-09-14 13:07:58 -0500

    Added annotator role check.

--- app/controllers/abstractController.js
+++ app/controllers/abstractController.js
@@ -16,12 +16,16 @@ core.controller('AbstractController', function ($scope, StorageService) {
 		return (sessionStorage.role == "ROLE_USER");
 	};
 
-	$scope.isAdmin = function() {
-		return (sessionStorage.role == "ROLE_ADMIN");
+	$scope.isAnnotator = function() {
+		return (sessionStorage.role == "ROLE_ANNOTATOR");
 	};
-	
+
 	$scope.isManager = function() {
 		return (sessionStorage.role == "ROLE_MANAGER");
 	};
-			
+
+	$scope.isAdmin = function() {
+		return (sessionStorage.role == "ROLE_ADMIN");
+	};
+
 });

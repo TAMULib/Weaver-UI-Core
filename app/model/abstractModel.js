56dfd8abc9ad1fc30bfc57308d1de41a6b6aaf40
William Welling <wwelling110381@outlook.com>
2015-10-05 15:29:57 -0500

    New abstract update method. Removed old message handling.

--- app/model/abstractModel.js
+++ app/model/abstractModel.js
@@ -9,25 +9,22 @@ core.service("AbstractModel", function () {
 	* All abstracted methods can go here: (e.g. AbstractModel.myMethod = funciton() {} )
 	* A model can then extend this my including "self = this;" and "angular.extend(self, AbstractModel);"
 	* in its contructor.
-	*/ 
-	
-	AbstractModel.unwrap = function(self, futureData, modelString, additionalMessage) {
+	*/
+	AbstractModel.unwrap = function(self, futureData, modelString) {
 		if(!futureData.$$state) {
 			angular.extend(self, futureData);
 			return;
 		}
 		futureData.then(
 			function(data) {
-				
 				if(data.body) {
 					angular.extend(self, JSON.parse(data.body).payload[modelString]);
-					if(JSON.parse(data.body).payload[additionalMessage]) {
-						angular.extend(self, {'message':JSON.parse(data.body).payload[additionalMessage]});
+					if(JSON.parse(data.body).meta.type == "ERROR") {
+						angular.extend(self, {'error':true});
 					}
 				} else {
 					angular.extend(self, data);
 				}
-								
 			},
 			function(data) {
 				console.error(data);
@@ -38,7 +35,19 @@ core.service("AbstractModel", function () {
 				}
 				else {
 					angular.extend(self, {'value':data});
-				}	
+				}
+		});
+	};
+
+	AbstractModel.update = function(self, promise, modelString) {
+		promise.then(function(data) {
+			if(typeof JSON.parse(data.body).payload[modelString] != 'undefined') {
+				self.unwrap(self, JSON.parse(data.body).payload[modelString], modelString);
+			}
+			else {
+				console.log(modelString + ' is undefined');
+				console.log(data);
+			}
 		});
 	};
 	

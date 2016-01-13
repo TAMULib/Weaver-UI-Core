9fee2bebb570441d6e2e29bd530a3e7a7693d48d
jeremythuff <jeremythuff@gmail.com>
2016-01-13 17:27:07 -0600

    Added ngdocs to the project

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,79 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "core",
+      "shortName": "core",
+      "type": "object",
+      "moduleName": "core",
+      "shortDescription": "This is the main module for the TAMU-UI-Core framework.",
+      "keywords": "angular api build core framework imported intended main module object tamu tamu-ui-core ui webservice work"
+    },
+    {
+      "section": "api",
+      "id": "core.controller:AbstractController",
+      "shortName": "AbstractController",
+      "type": "controller",
+      "moduleName": "core",
+      "shortDescription": "This abstract controller should be inherited by all controllers using",
+      "keywords": "$controller $scope $window abstract abstractcontroller access acutually adminstrator alert angular anonymously api assuming boolean browsing call control controller controllers core current error exposes extend extending functionality identity indicating inherited initiates isadmin isannotator isanonymous isassumed isassuming ismanager isuser method methods object process property report reporterror reporting rest returns role role_admin role_annotator role_manger role_user scope service storage storageservice tamu-ui-core user void webservice"
+    },
+    {
+      "section": "api",
+      "id": "core.controller:UserController",
+      "shortName": "UserController",
+      "type": "controller",
+      "moduleName": "core",
+      "shortDescription": "This controller sets the user&#39;s role in session storage. Extends &#39;AbstractController&#39;",
+      "keywords": "$controller $scope abstractcontroller api controller core extends ng-controller role service session sets storage user usercontroller"
+    },
+    {
+      "section": "api",
+      "id": "core.directive:modal",
+      "shortName": "modal",
+      "type": "directive",
+      "moduleName": "core",
+      "shortDescription": "The modal element directive is used to include a bootstrap style modal in your application.",
+      "keywords": "$controller api application bootstrap core directive element example html iews include modal modal-header-class modal-header-primary modal-id modal-view style"
+    },
+    {
+      "section": "api",
+      "id": "core.service:AbstractModel",
+      "shortName": "AbstractModel",
+      "type": "service",
+      "moduleName": "core",
+      "shortDescription": "This abstract model should be inherited by all models using",
+      "keywords": "absrtactmodel abstract abstracted abstractmodel angular api capabilites contructor core data exposes extend extended extending extends funciton future futuredata including inherited inheriting method methods model models mymethod object promise re-extends returns service tamu-ui-core unwrap unwrapping unwraps update void webservice"
+    },
+    {
+      "section": "api",
+      "id": "core.service:RestApi",
+      "shortName": "RestApi",
+      "type": "service",
+      "moduleName": "core",
+      "shortDescription": "The RestApi Service facilitates all restful communication",
+      "keywords": "$http $window anonymousget api application authenticated authservice behalf boolean communication configuration configured core counterpart facilitates functionality http implementation initiates isurl method object post private promise property reference req request restapi restful returns role role_anonymouse service spring tamu user web webservice websocket wsapi"
+    },
+    {
+      "section": "api",
+      "id": "coreConfig",
+      "shortName": "coreConfig",
+      "type": "object",
+      "moduleName": "coreConfig",
+      "shortDescription": "A configuration object that is included in &#39;core&#39; as a constant",
+      "keywords": "api configuration constant core coreconfig included object"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};

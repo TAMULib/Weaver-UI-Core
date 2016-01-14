e2a1a89d47e7c2ece9991f0de3d4f9ad86ce4f1f
Rincy <rmathew@library.tamu.edu>
2016-01-14 15:42:12 -0600

    Added comments to the Authentication Controller

--- app/controllers/authenticationController.js
+++ app/controllers/authenticationController.js
@@ -1,7 +1,36 @@
+/**
+ * @ngdoc controller
+ * @name core.controller:AuthenticationController
+ * @requires $controller
+ * @requires $location
+ * @requires $scope
+ * @requires $window
+ * @requires core.service:User
+ *
+ * @example
+ * The following must be added to authentication controller for it to extend AbstractController
+ *  <pre>
+ *      angular.extend(this, $controller('AbstractController', {$scope: $scope}));
+ *  </pre> 
+ *
+ * @description
+ *  This authentication controller contains login and logout methods. These methods based on user credentials 
+ *  will modify the user role and provide the necessary token to navigate to pages which require access based on roles.
+ *
+**/
 core.controller('AuthenticationController', function ($controller, $location, $scope, $window, User) {
 
     angular.extend(this, $controller('AbstractController', {$scope: $scope}));
-
+    /**
+     * @ngdoc method
+     * @name core.controller:AuthenticationController#$scope.login
+     * @methodOf core.controller:AuthenticationController
+     * @returns {void} returns void
+     * 
+     * @description
+     *  The login method deletes the default 'ROLE_ANONYMOUS' and its specific token based on user role found in 
+     *  user credentials.
+     */
     $scope.login = function(page) {
         
         delete sessionStorage.token;
@@ -29,6 +58,16 @@ core.controller('AuthenticationController', function ($controller, $location, $s
 
     };
 
+    /**
+     * @ngdoc method
+     * @name core.controller:AuthenticationController#$scope.logout
+     * @methodOf core.controller:AuthenticationController
+     * @returns {void} returns void
+     * 
+     * @description
+     *  The logout method will delete the session storage token, will set the sessionStorage role to default 'ROLE_ANONYMOUS' and will 
+     *  direct to the default home page
+     */
     $scope.logout = function() {
 
         delete sessionStorage.token;

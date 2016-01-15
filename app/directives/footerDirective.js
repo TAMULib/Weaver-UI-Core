75bc991a0515e39e8f652932e19cf36b692906b8
Rincy Mathew <rmathew@tamu.edu>
2016-01-15 17:40:46 -0600

    Work in Progress for angular documentation

--- app/directives/footerDirective.js
+++ app/directives/footerDirective.js
@@ -1,10 +1,34 @@
+/**
+ * @ngdoc directive
+ * @name  core.directive:tamufooter
+ * @restrict 'E'
+ * @requires $controller
+ * @scope
+ *
+ * @example
+ * <pre>
+ * 	<tamufooter></tamufooter>
+ * </pre>
+ * 
+ * @description 
+ *	The tamufooter element directive is used to includes a TAMU specific footer information provided in footer.html file.
+ * 
+ */
 core.directive('tamufooter', function ($controller) {
 	return {
 		templateUrl: 'bower_components/core/app/views/footer.html',
 		restrict: 'E',
 		replace: false,
 		transclude: true,
-		link: function ($scope, element, attr) {	    	
+		link: function ($scope, element, attr) {
+			/**
+			 * @ngdoc property
+			 * @name core.directives:tamufooter#$scope.attr
+			 * @propertyOf core.directives:tamufooter
+			 *
+			 * @description
+			 * 	A $scope variable to store the 'attr' object properties
+			 */		    	
 			$scope.attr = attr;			
 	    }
 	};

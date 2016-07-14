764518eca188becce7cbbe3629efe1f5751004f9
William Welling <wwelling@library.tamu.edu>
2016-07-14 15:29:27 -0500

    Replaced divs with spans for non blocking layout in validated input directive.

--- app/directives/validatedInputDirective.js
+++ app/directives/validatedInputDirective.js
@@ -10,10 +10,18 @@ core.directive("validatedinput", function() {
 			"placeholder": "@",
 			"truevalue": "@",
 			"falsevalue": "@",
+			"blur": "&",
 			"results": "="
 		},
 		link: function ($scope, element, attr) {
 			$scope.view = attr.view ? attr.view : "bower_components/core/app/views/directives/validatedInput.html";
+		
+			$scope.keydown = function($event) {
+				if($event.which == 13) {
+					$event.target.blur();
+				}
+			};
+
 		}
 	};
 });

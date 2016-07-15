ecf3bad4ff12f707dc3068ba0a7fa8328850546e
William Welling <wwelling@library.tamu.edu>
2016-07-15 16:15:12 -0500

    Improved validated input directive.

--- app/directives/validatedInputDirective.js
+++ app/directives/validatedInputDirective.js
@@ -9,24 +9,30 @@ core.directive("validatedinput", function() {
 			"label": "@",
 			"placeholder": "@",
 			"truevalue": "@",
-			"falsevalue": "@",
-			"blur": "&",
+			"falsevalue": "@",			
 			"blurEnabled": "=",
 			"form": "=",
+			"confirm": "&",
 			"validations": "=",
 			"results": "="
 		},
 		link: function ($scope, element, attr) {
 			$scope.view = attr.view ? attr.view : "bower_components/core/app/views/directives/validatedInput.html";
-			
+
 			$scope.keydown = function($event) {
 				// enter(13): submit value to be persisted
-				if($event.which == 13 && $scope.blurEnabled) {
-					$event.target.blur();
+				if($event.which == 13 && $scope.blurEnabled && $scope.form.$valid) {
+					$scope.confirm();
 				}
 				// escape(27): reset value using shadow
 				if($event.which == 27) {
 					$scope.model.refresh();
+				}
+			};
+
+			$scope.blur = function($event) {
+				if($scope.blurEnabled && $scope.form.$valid) {
+					$scope.confirm();
 				}
 			};
 

fc976d49f97fbd5d827edc8190abd1e6b3890b2c
William Welling <wwelling@library.tamu.edu>
2016-07-14 21:01:31 -0500

    Refresh model when pressing escape within validated input.

--- app/directives/validatedInputDirective.js
+++ app/directives/validatedInputDirective.js
@@ -17,8 +17,13 @@ core.directive("validatedinput", function() {
 			$scope.view = attr.view ? attr.view : "bower_components/core/app/views/directives/validatedInput.html";
 		
 			$scope.keydown = function($event) {
+				// enter(13): submit value to be persisted
 				if($event.which == 13) {
 					$event.target.blur();
+				}
+				// escape(27): reset value using shadow
+				if($event.which == 27) {
+					$scope.model.refresh();
 				}
 			};
 

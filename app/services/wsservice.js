a1796688c5755819b3336175c188fa2ba9b085d2
William Welling <wwelling@library.tamu.edu>
2017-02-17 09:22:27 -0600

    Show alert only once and after 2 minutes without response.

--- app/services/wsservice.js
+++ app/services/wsservice.js
@@ -5,226 +5,242 @@
  * @requires ng.$q
  * @requires core.service:AlertService
  *
- * @description 
+ * @description
  *  An angular service wrapper for stop communication over websockets.
- * 
+ *
  */
-core.service("WsService", function($interval, $q, AlertService) { 
-	
-	var WsService = this;
-	
-	/**
-	 * @ngdoc property
-	 * @name  core.service:WsService#WsService.pendingReqCounter
-	 * @propertyOf core.service:WsService
-	 *
-	 * @description 
-	 *  A count of all unresolved requests.
-	 * 
-	 */
-	WsService.pendingReqCounter = 0;
-	
-	/**
-	 * @ngdoc property
-	 * @name  core.service:WsService#WsService.pendingReq
-	 * @propertyOf core.service:WsService
-	 *
-	 * @description 
-	 *  An object store for pending requests.
-	 * 
-	 */
-	WsService.pendingReq = {};
-	
-	/**
-	 * @ngdoc property
-	 * @name  core.service:WsService#WsService.subscriptions
-	 * @propertyOf core.service:WsService
-	 *
-	 * @description 
-	 *  An object store for subscriptions.
-	 * 
-	 */
-	WsService.subscriptions = {};
+core.service("WsService", function($interval, $q, AlertService) {
 
-	/**
-	 * @ngdoc method
-	 * @name  core.service:WsService#WsService.subscribe
-	 * @methodOf core.service:WsService
-	 * @param {string} channel 
-	 *  The channel which will be subscribed to.
-	 * @param {boolean=} persist 
-	 *  Indicates that the subscribe channel should not be removed on view change.
-	 *
-	 * @description 
-	 *  Registers a subscription to a stomp channel.
-	 * 
-	 */
-	WsService.subscribe = function(channel, persist) {		
-		var id = "sub-" + window.stompClient.counter;		
-		var defer;
-		
-		if(!persist) persist = false;
-		
-		var subObj;
+  var WsService = this;
 
-		if((subObj = WsService.subExist(channel))) {
-			defer = subObj.defer;
-		} else {
-			
-			defer = $q.defer();
-			subObj = {
-				channel: channel,
-				defer: defer
-			};
+  /**
+   * @ngdoc property
+   * @name  core.service:WsService#WsService.requestCount
+   * @propertyOf core.service:WsService
+   *
+   * @description
+   *  A count of all requests.
+   *
+   */
+  WsService.requestCount = 0;
 
-			window.stompClient.subscribe(channel, function(data) {
-				
-				var meta = JSON.parse(data.body).meta
+  /**
+   * @ngdoc property
+   * @name  core.service:WsService#WsService.pendingReq
+   * @propertyOf core.service:WsService
+   *
+   * @description
+   *  An object store for pending requests.
+   *
+   */
+  WsService.pendingReq = {};
 
-				var requestId = meta.id ? meta.id : null;				
-				var response = meta.type;
+  /**
+   * @ngdoc property
+   * @name  core.service:WsService#WsService.delinquentReq
+   * @propertyOf core.service:WsService
+   *
+   * @description
+   *  An object store for delinquent requests.
+   *
+   */
+  WsService.delinquentReq = {};
 
-				if(WsService.pendingReq[requestId]) {
+  /**
+   * @ngdoc property
+   * @name  core.service:WsService#WsService.subscriptions
+   * @propertyOf core.service:WsService
+   *
+   * @description
+   *  An object store for subscriptions.
+   *
+   */
+  WsService.subscriptions = {};
 
-					//logger.info("");
-					//logger.debug(channel);
-					//logger.info("Resolving Request " + requestId + ": " + WsService.pendingReq[requestId].request);
-					//logger.log(JSON.parse(data.body));
-					
-					if(response == "REFRESH") {
-						WsService.pendingReq[requestId].defer.notify(data);
-					} else {
-						// We should always resolve to handle alternative notifications.
-						WsService.pendingReq[requestId].defer.resolve(data);
-						AlertService.add(meta, channel);
-						delete WsService.pendingReq[requestId];	
-					}
-					
-				}
-				
-				defer.notify(data);
+  /**
+   * @ngdoc method
+   * @name  core.service:WsService#WsService.subscribe
+   * @methodOf core.service:WsService
+   * @param {string} channel
+   *  The channel which will be subscribed to.
+   * @param {boolean=} persist
+   *  Indicates that the subscribe channel should not be removed on view change.
+   *
+   * @description
+   *  Registers a subscription to a stomp channel.
+   *
+   */
+  WsService.subscribe = function(channel, persist) {
+    var id = "sub-" + window.stompClient.counter;
+    var defer;
 
-			});
+    if (!persist) persist = false;
 
-			WsService.subscriptions[id] = subObj;
-		}
+    var subObj;
 
-		return defer.promise;
-	};
+    if ((subObj = WsService.subExist(channel))) {
+      defer = subObj.defer;
+    } else {
 
-	/**
-	 * @ngdoc method
-	 * @name  core.service:WsService#WsService.send
-	 * @methodOf core.service:WsService
-	 * @param {string} request 
-	 *  The destination of this request.
-	 * @param {object} headers
-	 *  An object containing desired stomp headers
-	 * @param {object} payload
-	 *  An objecct containing any data that should be communicated
-	 *  with the request
-	 * @param {string} channel
-	 *  The chennel which the reseponse will be communicated over.
-	 *
-	 * @description 
-	 *  Sends a websocket message.
-	 * 
-	 */
-	WsService.send = function(request, headers, payload, channel) {
+      defer = $q.defer();
+      subObj = {
+        channel: channel,
+        defer: defer
+      };
 
-		if(!WsService.subExist(channel)) {
-			var endpoint = channel; 
-			var controller = channel.substr(0, channel.lastIndexOf("/"));
-			AlertService.create(endpoint);
-			AlertService.create(controller);
-			WsService.subscribe(channel);
-		}
-		
-		headers.id = WsService.pendingReqCounter++;
+      window.stompClient.subscribe(channel, function(data) {
 
-		window.stompClient.send(request, headers, payload);
-		
-		WsService.pendingReq[headers.id] = {
-			defer: $q.defer(),
-			timestamp: new Date().getTime(),
-			request: request,
-			resend: function() {
-				headers.jwt = sessionStorage.token;
-				window.stompClient.send(request, headers, payload);
-			}
-		};
+        var meta = JSON.parse(data.body).meta
 
-		return WsService.pendingReq[headers.id].defer.promise;
-		
-	};
+        var requestId = meta.id ? meta.id : null;
+        var response = meta.type;
 
-	/**
-	 * @ngdoc method
-	 * @name  core.service:WsService#WsService.subExist
-	 * @methodOf core.service:WsService
-	 * @param {string} channel 
-	 *  The channel which is being confirmed.
-	 * @returns {boolean|object} 
-	 *  Returns either false, or the subscription object 
-	 *  associated with the indicated channel.
-	 * @description 
-	 *  Both requests and confirms the existense of a specific 
-	 *  subscription.
-	 * 
-	 */
-	WsService.subExist = function(channel) {
-		for(var key in WsService.subscriptions) {
-			var subObj = WsService.subscriptions[key];
-			if(subObj.channel == channel) return subObj;
-		}
-		return false;
-	};
-	
-	/**
-	 * @ngdoc method
-	 * @name  core.service:WsService#WsService.unsubscribe
-	 * @methodOf core.service:WsService
-	 * @param {object} sub 
-	 *  The subscription which will be unsubscribed to.
-	 * @returns {void} returns void
-	 *
-	 *  @description 
-	 *   Unsubscribes from the indicated subscription.
-	 * 
-	 */
-	WsService.unsubscribe = function(sub) {
-		window.stompClient.unsubscribe(sub);
-		delete WsService.subscriptions[sub];
-	};
+        if (WsService.pendingReq[requestId]) {
 
-	/**
-	 * @ngdoc method
-	 * @name  core.service:WsService#WsService.unsubscribeAll
-	 * @methodOf core.service:WsService
-	 * @returns {void} returns void
-	 *
-	 *  @description 
-	 *   Unsubscribes from all subscriptions.
-	 * 
-	 */
-	WsService.unsubscribeAll = function() {
-		for(var key in WsService.subscriptions){
-			var sub = WsService.subscriptions[key];
-			if(!sub.persist) WsService.unsubscribe(key);
-		}
-	};
+          //logger.info("");
+          //logger.debug(channel);
+          //logger.info("Resolving Request " + requestId + ": " + WsService.pendingReq[requestId].request);
+          //logger.log(JSON.parse(data.body));
 
-	$interval(function() {
+          if (response == "REFRESH") {
+            WsService.pendingReq[requestId].defer.notify(data);
+          } else {
+            // We should always resolve to handle alternative notifications.
+            WsService.pendingReq[requestId].defer.resolve(data);
+            AlertService.add(meta, channel);
+            delete WsService.pendingReq[requestId];
+          }
 
-		var now = new Date().getTime();
+        }
 
-		for(var req in WsService.pendingReq) {
-			if(now - WsService.pendingReq[req].timestamp > 60000) {
-				console.log(WsService.pendingReq);
-				AlertService.add({type: "WARNING", message: "Web service is taking too long to respond. Please refresh. If this continues to appear you can email helpdesk@library.tamu.edu."}, "/app/warnings");  
-			} 
-		}
+        defer.notify(data);
 
-	}, 10000);
+      });
+
+      WsService.subscriptions[id] = subObj;
+    }
+
+    return defer.promise;
+  };
+
+  /**
+   * @ngdoc method
+   * @name  core.service:WsService#WsService.send
+   * @methodOf core.service:WsService
+   * @param {string} request
+   *  The destination of this request.
+   * @param {object} headers
+   *  An object containing desired stomp headers
+   * @param {object} payload
+   *  An objecct containing any data that should be communicated
+   *  with the request
+   * @param {string} channel
+   *  The chennel which the reseponse will be communicated over.
+   *
+   * @description
+   *  Sends a websocket message.
+   *
+   */
+  WsService.send = function(request, headers, payload, channel) {
+
+    if (!WsService.subExist(channel)) {
+      var endpoint = channel;
+      var controller = channel.substr(0, channel.lastIndexOf("/"));
+      AlertService.create(endpoint);
+      AlertService.create(controller);
+      WsService.subscribe(channel);
+    }
+
+    headers.id = WsService.requestCount++;
+
+    window.stompClient.send(request, headers, payload);
+
+    WsService.pendingReq[headers.id] = {
+      defer: $q.defer(),
+      timestamp: new Date().getTime(),
+      request: request,
+      resend: function() {
+        headers.jwt = sessionStorage.token;
+        window.stompClient.send(request, headers, payload);
+      }
+    };
+
+    return WsService.pendingReq[headers.id].defer.promise;
+
+  };
+
+  /**
+   * @ngdoc method
+   * @name  core.service:WsService#WsService.subExist
+   * @methodOf core.service:WsService
+   * @param {string} channel
+   *  The channel which is being confirmed.
+   * @returns {boolean|object}
+   *  Returns either false, or the subscription object
+   *  associated with the indicated channel.
+   * @description
+   *  Both requests and confirms the existense of a specific
+   *  subscription.
+   *
+   */
+  WsService.subExist = function(channel) {
+    for (var key in WsService.subscriptions) {
+      var subObj = WsService.subscriptions[key];
+      if (subObj.channel == channel) return subObj;
+    }
+    return false;
+  };
+
+  /**
+   * @ngdoc method
+   * @name  core.service:WsService#WsService.unsubscribe
+   * @methodOf core.service:WsService
+   * @param {object} sub
+   *  The subscription which will be unsubscribed to.
+   * @returns {void} returns void
+   *
+   *  @description
+   *   Unsubscribes from the indicated subscription.
+   *
+   */
+  WsService.unsubscribe = function(sub) {
+    window.stompClient.unsubscribe(sub);
+    delete WsService.subscriptions[sub];
+  };
+
+  /**
+   * @ngdoc method
+   * @name  core.service:WsService#WsService.unsubscribeAll
+   * @methodOf core.service:WsService
+   * @returns {void} returns void
+   *
+   *  @description
+   *   Unsubscribes from all subscriptions.
+   *
+   */
+  WsService.unsubscribeAll = function() {
+    for (var key in WsService.subscriptions) {
+      var sub = WsService.subscriptions[key];
+      if (!sub.persist) WsService.unsubscribe(key);
+    }
+  };
+
+  $interval(function() {
+
+    var now = new Date().getTime();
+
+    for (var req in WsService.pendingReq) {
+      if (now - WsService.pendingReq[req].timestamp > 120000) {
+        if (WsService.delinquentReq[req] === undefined) {
+          WsService.delinquentReq[req] = WsService.pendingReq[req];
+          AlertService.add({
+            type: "WARNING",
+            message: "Web service is taking too long to respond. Please refresh. If this continues to appear you can email helpdesk@library.tamu.edu."
+          }, "/app/warnings");
+        }
+      }
+    }
+
+  }, 10000);
 
 });

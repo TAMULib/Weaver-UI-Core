3f5ca753ab96f2c42a76ad3616e4fd9147d077e4
William Welling <wwelling@library.tamu.edu>
2017-03-03 12:09:07 -0600

    Make copy of request to not mutate apiMapping.

--- app/services/wsapi.js
+++ app/services/wsapi.js
@@ -8,13 +8,13 @@
  *
  * @description
  *  A service wrapper for the webservices api.
- * 
+ *
  */
 core.service("WsApi", function($q, $http, WsService, AuthServiceApi) {
 
-	var WsApi = this;
+    var WsApi = this;
 
-	/**
+    /**
 	 * @ngdoc method
 	 * @name  core.service:WsApi#WsApi.listen
 	 * @methodOf core.service:WsApi
@@ -22,23 +22,23 @@ core.service("WsApi", function($q, $http, WsService, AuthServiceApi) {
 	 *  An apireq which containes the channel, controller and method
 	 *  which should be listened to.
 	 * @returns {Promsie} A promise from a websocket subscription subscription
-	 *  
-	 * @description 
-	 *  This method gives a promise which is notified upon 
+	 *
+	 * @description
+	 *  This method gives a promise which is notified upon
 	 *  websocket communication on the desired channel
-	 * 
+	 *
 	 */
-	WsApi.listen = function(apiReq) {
-		var request = '/ws/' + apiReq.controller + '/' + apiReq.method;
-		var channel = apiReq.endpoint + "/" + apiReq.controller;
-		
-		if(apiReq.method) {
-			channel +=  "/" + apiReq.method;
-		}
-		return WsService.subscribe(channel);
-	};
+    WsApi.listen = function(apiReq) {
+        var request = '/ws/' + apiReq.controller + '/' + apiReq.method;
+        var channel = apiReq.endpoint + "/" + apiReq.controller;
 
-	/**
+        if (apiReq.method) {
+            channel += "/" + apiReq.method;
+        }
+        return WsService.subscribe(channel);
+    };
+
+    /**
 	 * @ngdoc method
 	 * @name  core.service:WsApi#WsApi.fetch
 	 * @methodOf core.service:WsApi
@@ -46,42 +46,58 @@ core.service("WsApi", function($q, $http, WsService, AuthServiceApi) {
 	 *  An apireq which containes the channel, controller and method
 	 *  which should be listened to.
 	 * @returns {Promsie} A promise from the WsService send method
-	 *  
-	 * @description 
-	 *  This method gives a promise which is resolved by id upon 
+	 *
+	 * @description
+	 *  This method gives a promise which is resolved by id upon
 	 *  websocket communication on the desired channel
-	 * 
+	 *
 	 */
-	WsApi.fetch = function(apiReq) {
+    WsApi.fetch = function(initialReq, manifest) {
 
-		var request = '/ws/' + apiReq.controller + '/' + apiReq.method;	  
-		var channel = apiReq.endpoint + "/" + apiReq.controller + "/" + apiReq.method;
+        var apiReq = angular.copy(initialReq);
 
-		var headers = {
-			'jwt': sessionStorage.token, 
-			'data': (typeof apiReq.data != 'undefined') ? JSON.stringify(apiReq.data) : '{}'
-		};
+        if (manifest && manifest.pathValues) {
 
-		var fetchPromise = WsService.send(request, headers, {}, channel);
+            for (var key in manifest.pathValues) {
+                var value = manifest.pathValues[key];
+                apiReq.method = apiReq.method.replace(new RegExp(':' + key, 'g'), value);
+            }
+        }
 
-		fetchPromise.then(null, null, function(data) {
- 
-			var meta = JSON.parse(data.body).meta;
+        if (manifest && manifest.data) {
+            apiReq.data = manifest.data;
+        }
 
-			if(meta.type == "REFRESH") {
-				if(sessionStorage.assumedUser) {					
-					AuthServiceApi.getAssumedUser(JSON.parse(sessionStorage.assumedUser)).then(function() {
-						WsService.pendingReq[meta.id].resend();
-					});
-				} else {
-					AuthServiceApi.getRefreshToken().then(function() {
-						WsService.pendingReq[meta.id].resend();
-					});
-				}
-			}
-		});
+        var request = '/ws/' + apiReq.controller + '/' + apiReq.method;
+        var channel = apiReq.endpoint + "/" + apiReq.controller + "/" + apiReq.method;
 
-		return fetchPromise;
-	};
-	
+        var headers = {
+            'jwt': sessionStorage.token,
+            'data': (typeof apiReq.data != 'undefined')
+                ? JSON.stringify(apiReq.data)
+                : '{}'
+        };
+
+        var fetchPromise = WsService.send(request, headers, {}, channel);
+
+        fetchPromise.then(null, null, function(data) {
+
+            var meta = JSON.parse(data.body).meta;
+
+            if (meta.type == "REFRESH") {
+                if (sessionStorage.assumedUser) {
+                    AuthServiceApi.getAssumedUser(JSON.parse(sessionStorage.assumedUser)).then(function() {
+                        WsService.pendingReq[meta.id].resend();
+                    });
+                } else {
+                    AuthServiceApi.getRefreshToken().then(function() {
+                        WsService.pendingReq[meta.id].resend();
+                    });
+                }
+            }
+        });
+
+        return fetchPromise;
+    };
+
 });

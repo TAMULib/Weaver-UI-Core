17bc736b4b72d1b1260c655bfbd4a01a466b4d05
William Welling <wwelling@library.tamu.edu>
2017-08-09 16:33:48 -0500

    format files

--- cli/server.js
+++ cli/server.js
@@ -9,37 +9,39 @@ const router = express.Router();
 
 // The run command
 function run(args) {
-  if(!args["-h"]) {
-    
-    const port = args["-p"] ? args["-p"] : 8080;
-    const base = args["-b"] ? args["-b"] : '/';
+    if (!args["-h"]) {
 
-    const root = process.cwd() + path.sep + 'app';
+        const port = args["-p"] ? args["-p"] : 8080;
+        const base = args["-b"] ? args["-b"] : '/';
 
-    router.use(express.static('app'));
+        const root = process.cwd() + path.sep + 'app';
 
-    router.use(function(req, res, next) {
-      if(req.get("X-Requested-With")) {
-        res.status(404).send('Not found');
-      } else {
-        res.sendFile("index.html", { root: root });
-      }
-    });
+        router.use(express.static('app'));
 
-    app.use(base, router);
-    
-    app.listen(port, function () {
-      console.log('listening on port ' + port + ' at ' + base);
-    });
+        router.use(function (req, res, next) {
+            if (req.get("X-Requested-With")) {
+                res.status(404).send('Not found');
+            } else {
+                res.sendFile("index.html", {
+                    root: root
+                });
+            }
+        });
 
-  } else {
-    help();
-  }
+        app.use(base, router);
+
+        app.listen(port, function () {
+            console.log('listening on port ' + port + ' at ' + base);
+        });
+
+    } else {
+        help();
+    }
 }
 
 // The help text
 function help() {
-  console.log("wrv server [-h, -p [port], -b [base url]]");
+    console.log("wrv server [-h, -p [port], -b [base url]]");
 }
 
 exports.run = run;
--- cli/weaverCli.js
+++ cli/weaverCli.js
@@ -8,36 +8,36 @@ const command = process.argv[2];
 const commandArgsArr = process.argv.slice(3, process.argv.length);
 const commandArgs = {};
 
-for(var i = 0; i < commandArgsArr.length; i++) {
-  const cmd = commandArgsArr[i];
-  if(cmd[0] === '-') {
-    var nextEntry = commandArgsArr[i + 1];
-    if(nextEntry && nextEntry[0] !== '-') {
-      commandArgs[cmd] = nextEntry;
-    } else {
-      commandArgs[cmd] = true;
+for (var i = 0; i < commandArgsArr.length; i++) {
+    const cmd = commandArgsArr[i];
+    if (cmd[0] === '-') {
+        var nextEntry = commandArgsArr[i + 1];
+        if (nextEntry && nextEntry[0] !== '-') {
+            commandArgs[cmd] = nextEntry;
+        } else {
+            commandArgs[cmd] = true;
+        }
     }
-  } 
 }
 
 const commands = {
-  server: require('./server.js')
+    server: require('./server.js')
 }
 
-if(commands[command]) {
-  //If the command exists, run it
-  commands[command].run(commandArgs);
+if (commands[command]) {
+    //If the command exists, run it
+    commands[command].run(commandArgs);
 } else if (command === '-h') {
-  // If the command did not exists but is the '-h' flag, run help on all commands
-  console.log('COMMANDS:');
-  var cmdkeys = Object.keys(commands);
+    // If the command did not exists but is the '-h' flag, run help on all commands
+    console.log('COMMANDS:');
+    var cmdkeys = Object.keys(commands);
 
-  for(var i in cmdkeys) {
-    var cmd = commands[cmdkeys[i]];
-    cmd.help();
-  }
+    for (var i in cmdkeys) {
+        var cmd = commands[cmdkeys[i]];
+        cmd.help();
+    }
 } else {
-  // Finnally, show an error 
-  console.log('ERROR: '+ command +' not found.');
-  console.log('-h for help');
+    // Finnally, show an error
+    console.log('ERROR: ' + command + ' not found.');
+    console.log('-h for help');
 }

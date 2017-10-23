5fb7a8394895525613a27ef28a94dc504fe9fae2
William Welling <wwelling@outlook.com>
2017-10-22 23:58:21 -0500

    strong conditioning

--- app/services/userService.js
+++ app/services/userService.js
@@ -11,7 +11,7 @@ core.service("UserService", function ($q, StorageService, User, WsApi) {
         WsApi.fetch(currentUser.getMapping().instantiate).then(function (res) {
             delete sessionStorage.role;
             var credentials = angular.fromJson(res.body).payload.Credentials;
-            currentUser.anonymous = credentials.role == appConfig.anonymousRole ? true : false;
+            currentUser.anonymous = credentials.role === appConfig.anonymousRole ? true : false;
             angular.extend(currentUser, credentials);
             StorageService.set("role", currentUser.role);
             userEvents.notify('RECEIVED');

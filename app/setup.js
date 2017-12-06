f7a865bb57c3451530128730b9e433acd116c682
William Welling <wwelling@library.tamu.edu>
2017-12-06 13:12:26 -0600

    better query param getter

--- app/setup.js
+++ app/setup.js
@@ -95,26 +95,23 @@ function setUpApp(bootstrapApp) {
         });
     };
 
-    function getParameterByName(name, url) {
-        if (!url) {
-          url = window.location.href;
+    function getQueryVariable(variable) {
+      var query = window.location.search.substring(1);
+      var vars = query.split("&");
+      for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] === variable){
+          return pair[1];
         }
-        name = name.replace(/[\[\]]/g, "\\$&");
-        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), results = regex.exec(url);
-        if (!results) {
-          return null;
-        }
-        if (!results[2]) {
-          return '';
-        }
-        return decodeURIComponent(results[2].replace(/\+/g, " "));
+      }
+      return(false);
     }
 
     function getJwt() {
         if (sessionStorage.token) {
             return sessionStorage.token;
         }
-        return getParameterByName('jwt');
+        return getQueryVariable('jwt');
     };
 
     function cleanUrl() {

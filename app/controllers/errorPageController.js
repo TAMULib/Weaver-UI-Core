034d9b7ffec26c1e92d997703775159b2ea0de25
Kevin Day <kday@library.tamu.edu>
2022-12-08 11:22:48 -0600

    Fix tabbing to align with 2-space-tab practices.

--- app/controllers/errorPageController.js
+++ app/controllers/errorPageController.js
@@ -1,25 +1,25 @@
 core.controller("ErrorPageController", function ($scope, AccessControlService) {
 
-    $scope.getPath = function (path) {
+  $scope.getPath = function (path) {
 
-      if (!!path && path.length > 0) {
-        var paths = path.split('/');
+    if (!!path && path.length > 0) {
+      var paths = path.split('/');
 
-        for (var i in paths) {
-          // Return to top-level when previous path contains route variables.
-          if (paths[i][0] === ':') {
-            paths = [];
-            break;
-          }
+      for (var i in paths) {
+        // Return to top-level when previous path contains route variables.
+        if (paths[i][0] === ':') {
+          paths = [];
+          break;
         }
-
-        return paths.join('/');
       }
 
-      return path;
-    };
+      return paths.join('/');
+    }
 
-    // This is actually a URL path and not a route.
-    $scope.lastRoute = $scope.getPath(AccessControlService.getLastRoutePath());
+    return path;
+  };
+
+  // This is actually a URL path and not a route.
+  $scope.lastRoute = $scope.getPath(AccessControlService.getLastRoutePath());
 
 });

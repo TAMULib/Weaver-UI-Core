4ff9edb40acdeedf86884b4d9b22bf653c4d3d33
William Welling <wwelling@library.tamu.edu>
2023-04-17 10:35:40 -0500

    Default data-keyboard to true to match bootstrap
    Still requires listeners as bootstrap 3 modal and/or weaver modal directive is not working as expected.
    
    Focus on first input in modal on open

--- app/services/modalService.js
+++ app/services/modalService.js
@@ -2,16 +2,34 @@ core.service("ModalService", function () {
 
     var ModalService = this;
 
+    var body = document.querySelector('body');
+
     var modalElement;
+
+    var closeOnEscape = function(event) {
+        if (modalElement && event.code === 'Escape') {
+            ModalService.closeModal();
+            event.preventDefault();
+        }
+    };
 
     ModalService.openModal = function (id) {
         modalElement = angular.element(id);
         modalElement.modal('show');
+        modalElement.on('shown.bs.modal', function (e) {
+            modalElement.find(":input:not(:button):visible:enabled:not([readonly]):first").focus();
+        });
+        if (modalElement.data('keyboard')) {
+            body.addEventListener('keydown', closeOnEscape);
+        }
     };
 
     ModalService.closeModal = function () {
         if (modalElement) {
             modalElement.modal('hide');
+            if (modalElement.data('keyboard')) {
+                body.removeEventListener('keydown', closeOnEscape);
+            }
         }
     };
 

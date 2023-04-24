e49208b118390075df3ee2fa20fc30d231733015
William Welling <wwelling@outlook.com>
2023-04-24 10:42:02 -0500

    Use single keydown event handle applied only when modal open

--- app/services/modalService.js
+++ app/services/modalService.js
@@ -5,38 +5,33 @@ core.service("ModalService", function ($timeout) {
     var bodyElement = angular.element('body');
 
     bodyElement.on('keydown', function (event) {
-        if (modalElement && event.code === 'Tab') {
-            $timeout(function () {
-                if (!modalElement.has(angular.element(':focus')).length) {
-                    modalElement.find(':input:first').focus();
-                }
-            });
+        if (modalElement) {
+            if (event.code === 'Tab') {
+                $timeout(function () {
+                    if (!modalElement.has(angular.element(':focus')).length) {
+                        modalElement.find(':input:first').focus();
+                    }
+                });
+            } else if (event.code === 'Escape') {
+                event.preventDefault();
+                ModalService.closeModal();
+            }
         }
     });
 
     var modalElement;
-
-    var handleKeydown = function (event) {
-        if (event.code === 'Escape') {
-            event.preventDefault();
-            ModalService.closeModal();
-        }
-    };
 
     ModalService.openModal = function (id) {
         modalElement = angular.element(id);
         modalElement.modal('show');
         modalElement.on('shown.bs.modal', function (e) {
             modalElement.find(':input:not(:button):visible:enabled:not([readonly]):first, :input:first').focus();
-
-            modalElement.on('keydown', handleKeydown);
         });
     };
 
     ModalService.closeModal = function () {
         if (modalElement) {
             modalElement.modal('hide');
-            modalElement.off('keydown');
             modalElement.off('shown.bs.modal');
             modalElement = undefined;
         }

d9a81564f0d9c755f393c498a77493f433c666fc
Jason Savell <jsavell@tamu.edu>
2025-03-12 16:34:14 -0500

    check for download error response in arraybuffer

--- app/services/fileService.js
+++ app/services/fileService.js
@@ -100,10 +100,23 @@ core.service("FileService", function ($http, $q, AlertService, AuthService, Uplo
                     },
                     // error callback
                     function (error) {
+                        let errorMessage = "";
+                        if (error.data instanceof ArrayBuffer) {
+                            let decoder = new TextDecoder("utf-8");
+                            let result = decoder.decode(error.data);
+                            try {
+                                apiResponse = JSON.parse(result);
+                                errorMessage = apiResponse.meta.message;
+                                error.data.message = errorMessage;
+                            } catch (e) {
+                                console.log(e);
+                            }
+                        }
                         AlertService.addAlertServiceError(error);
                         return {
                             meta: {
-                                status: 'ERROR'
+                                status: 'ERROR',
+                                message: errorMessage
                             },
                             payload: error.data
                         };
